4c7194b35d1ffabea6ce1079c8c792c4b5c2790a
Christina Lambert <clambert0615@gmail.com>
2020-10-29 16:50:36 -0400

    Worked on controller and some views

--- Hangman/Controllers/HangmanController.cs
+++ Hangman/Controllers/HangmanController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Hangman.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,7 @@ namespace Hangman.Controllers
         private readonly HangmanContext _context;
         private readonly APIInterface _api;
         public Words w = new Words();
+        public Guess g = new Guess();
         public HangmanController(HangmanContext context, APIInterface api)
         {
             _context = context;
@@ -30,5 +32,29 @@ namespace Hangman.Controllers
             return View(w);
         }
         
+        public IActionResult Guess(string word, char guess, int guessesLeft)
+        {
+            StringBuilder encrypted = new StringBuilder(word.Length);
+            List<char> guessed = new List<char>();
+           for (int i = 0; i < word.Length; i++)
+            {
+                encrypted.Append('*');
+            }
+            for (int j = 0; j < word.Length; j++)
+            {
+                if(guess == word[j])
+                {
+                    encrypted[j] = guess;
+                }
+            }
+            guessed.Add(guess);
+            guessesLeft -= 1; 
+            g.Guessed = guessed;
+            g.Word = word;
+            g.EncryptedWord = encrypted;
+            g.GuessesLeft = guessesLeft;
+            return View(g);
+        }
+
     }
 }
---
+++ Hangman/Models/Guess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Models
+{
+    public class Guess
+    {
+        public string Word { get; set; }
+        public StringBuilder EncryptedWord { get; set; }
+        public List<char>  Guessed { get; set; }
+
+        public int GuessesLeft { get; set; }
+    }
+}
